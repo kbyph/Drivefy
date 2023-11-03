0d2942b3651b03623c879cbce054390d8592c098
Kobe Pho <kobeypho@gmail.com>
2023-11-02 17:53:52 -0700

    Typo Correction

--- app/page.tsx
+++ app/page.tsx
@@ -52,7 +52,7 @@ export default function Home() {
         className="mt-12 padding-x padding-y max-width" 
         id="discover">
           <div className="home__text-container">
-            <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
+            <h1 className="text-4xl font-extrabold">Car Catalog</h1>
             <p>Explore the cars you might like!</p>
           </div>
 
