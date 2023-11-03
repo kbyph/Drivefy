4ff0c185aa45a7d8161204baccf92cad3c368f8e
Kobe Pho <kobeypho@gmail.com>
2023-11-02 17:50:38 -0700

    Typo Correction

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -16,7 +16,7 @@ const Hero = () => {
             </h1>
 
             <p className="hero__subtitle">
-                Streamline your car rental experience wih our effortless booking process.
+                Streamline your car rental experience with our effortless booking process.
             </p>
 
             <CustomButton 
